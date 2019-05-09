0afd4d53aa24a227967303ea00d1217595dcf7eb
Rushani Shawuti <ruhsane924@gmail.com>
2019-05-09 13:42:56 -0700

    scenario1 done

--- scenario1.py
+++ scenario1.py
@@ -1,2 +1,15 @@
-f = open('', 'r')
 
+# file = open('file.txt')
+
+# phone = "+8197753"
+
+# for line in file:
+#     line = line.strip().split(',')[0]
+#     if term == line[0]:             # <--- You can also stay with "if term in line:" if you doesn't care which field the "model" is. 
+#     print(line)
+# file.close()
+
+'''
+Use command: grep -r "prefix" file.txt 
+in terminal to quickly find that phone number's routing cost
+'''
