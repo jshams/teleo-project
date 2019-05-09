0b70369fd6aab6e3bdcb45cc72901d09bab99aaa
Rushani Shawuti <ruhsane924@gmail.com>
2019-05-09 15:17:05 -0700

    impelemnted look up dict methods

--- scenario2.py
+++ scenario2.py
@@ -3,8 +3,11 @@ class LookUpDict():
     def __init__(self, route_cost_path, phone_number_path):
         self.route_cost_path = route_cost_path
         self.phone_number_path = phone_number_path
+        self.longest_pref = None
+        self.shortest_pref = None
 
         self.route_cost_dict = self.route_cost_to_dict()
+        self.result = self.lookup_all_numbers()
 
     def route_cost_to_dict(self):
         '''
@@ -22,10 +25,60 @@ class LookUpDict():
             cost = splitted_line[1]
             route_dict[prefix] = cost
 
+            if self.longest_pref is None:
+                self.longest_pref = len(prefix)
+                self.shortest_pref = len(prefix)
+            else:
+                if len(prefix) < self.shortest_pref:
+                    self.shortest_pref = len(prefix)
+                if len(prefix) > self.longest_pref:
+                    self.longest_pref = len(prefix)
+
         file.close()
-        
+
         return route_dict
 
+    def lookup_number(self, num):
+        '''Given a full number search our dictionary for the cost or return None if not found'''
+        # search for full number in dict
+        # if not found, remove the last char
+        # OPTIMIZATION, the length of largest prefix is the index we should start at as long as len(num) < largest_pref_len
+        print(num)
+        if len(num) > self.longest_pref:
+            num = num[:self.longest_pref]
+        if len(num) < self.shortest_pref:
+            print("50 None")
+            return None
+
+        if num in self.route_cost_dict:
+            print('54', self.route_cost_dict[num])
+            return self.route_cost_dict[num]
+        else:
+            self.lookup_number(num[:-1])
+
+    def lookup_all_numbers(self):
+        '''Go through phone number path an find the cost of each phone number 
+        Use the lookup_number method to look through dictionary'''
+        
+        result = {}
+
+        file = open(self.phone_number_path)
+
+        for line in file:
+            number = line.strip()
+            cost = self.lookup_number(number)
+            print('70' ,cost)
+            result[number] = cost
+        file.close()
+
+        return result
+
 if __name__ == "__main__":
-    test_dict = LookUpDict("teleo.txt", "hello")
-    print(test_dict.route_cost_dict)
+    test_dict = LookUpDict("route-costs-1000000.txt", "phone.txt")
+    # print(test_dict.route_cost_dict)
+    # val = test_dict.lookup_number("+56r78")
+    # print(val)
+    result = test_dict.result
+    print(result)
+    print(test_dict.longest_pref)
+    print(test_dict.shortest_pref)
