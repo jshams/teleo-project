aba20e2cf61dd0ead222a2afa104dafc519d44b2
Rushani Shawuti <ruhsane924@gmail.com>
2019-05-09 14:13:33 -0700

    converted route cost to dictionary

--- scenario2.py
+++ scenario2.py
@@ -1,10 +1,31 @@
+class LookUpDict():
 
-file = open('file.txt')
+    def __init__(self, route_cost_path, phone_number_path):
+        self.route_cost_path = route_cost_path
+        self.phone_number_path = phone_number_path
 
-phone = "+8197753"
+        self.route_cost_dict = self.route_cost_to_dict()
 
-for line in file:
-    line = line.strip().split(',')[0]
-    if term == line[0]:             # <--- You can also stay with "if term in line:" if you doesn't care which field the "model" is. 
-    print(line)
-file.close()
+    def route_cost_to_dict(self):
+        '''
+        This method will convert our route cost text file into dictionary{prefix: cost}
+        Time complexity: O(n) worst and best case
+        Memroy complexity: O(n) worst and best
+        '''
+        route_dict = {}
+
+        file = open(self.route_cost_path)
+
+        for line in file:
+            splitted_line = line.strip().split(',')
+            prefix = splitted_line[0]
+            cost = splitted_line[1]
+            route_dict[prefix] = cost
+
+        file.close()
+        
+        return route_dict
+
+if __name__ == "__main__":
+    test_dict = LookUpDict("teleo.txt", "hello")
+    print(test_dict.route_cost_dict)
