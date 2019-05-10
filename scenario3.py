bc9b626035ff11346774551a2294bc1d1a597eb4
Jake Shams <jshams62@gmail.com>
2019-05-09 19:45:30 -0700

    find cost of num method

--- scenario3.py
+++ scenario3.py
@@ -10,7 +10,7 @@ class DecimalTreeNode():
         if self.children[num] is None:
             self.children[num] = DecimalTreeNode()
         if cost is not None:
-            self.children[num].cost = cost
+            self.cost = cost
 
 class DecimalTree():
     def __init__(self, route_cost_path, phone_number_path):
@@ -37,11 +37,24 @@ class DecimalTree():
         node = self.root
         for i, num in enumerate(prefix):
             num = int(num)
-            if i <= len(prefix) - 1:
+            if i < len(prefix) - 1:
                 node.insert(num)
                 node = node.children[num]
             else:
-                self.insert(num, cost)
+                node.insert(num, cost)
+    
+    def find_cost_of_num(self, number):
+        node = self.root
+        cost = None
+        for i, digit in enumerate(number):
+            if node.cost is not None:
+                cost = node.cost
+            if node.children[int(digit)] is None: # in the case that the number is larger than its prefix
+                return cost
+            node = node.children[int(digit)]
+        # in the case that the number is the same size as its prefix
+        return cost
 
 if __name__ == '__main__':
     test_dict = DecimalTree("route-costs-10.txt", "phone.txt")
+    cost = test_dict.find_cost_of_num("123")
