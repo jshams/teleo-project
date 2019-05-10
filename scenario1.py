0a69dab703ef30c7962c420e67018df3eab5c802
Jake Shams <jshams62@gmail.com>
2019-05-09 17:36:11 -0700

    reword

--- scenario1.py
+++ scenario1.py
@@ -1,4 +1,5 @@
 '''
-Use command: grep -r "[prefix]" "[file.txt]"
-in terminal to quickly find that phone number's routing cost
+Use a Bash Strip
+Use cli command: $ grep -r "[prefix]" "[path/to/file.txt]"
+this will show you the line its on (not the line number but what is on the line)
 '''
