55bbc21bcf1a35687294dc36102a40d2cfd465af
Jake Shams <jshams62@gmail.com>
2019-05-09 18:26:26 -0700

    implemented tree build methods

--- scenario3.py
+++ scenario3.py
@@ -1,27 +1,39 @@
 class DecimalTreeNode():
-    def __init__(self, data):
-        self.data = data
+    def __init__(self):
         self.children = [None] * 10
         self.cost = None
+    
+    def insert(self, num, cost = None):
+        if self.children[num] is None:
+            self.children[num] = DecimalTreeNode()
+        if cost is not None:
+            self.children[num].cost = cost
 
 class DecimalTree():
     def __init__(self, route_cost_path, phone_number_path):
         self.route_cost_path = route_cost_path
         self.phone_number_path = phone_number_path
-        
-        self.root = DecimalTreeNode('+')
+        self.root = DecimalTreeNode()
+    
+    def _slice_prefix_and_cost(self, line):
+        splitted_line = line.strip().split(',')
+        prefix = splitted_line[0][1:]
+        cost = splitted_line[1]
+        return prefix, cost
 
-    def populate_tree(self, prefix, cost):
-        pass
+    def populate_tree(self):
+        file = open(self.route_cost_path)
+        for line in file:
+            item = self._slice_prefix_and_cost(line)
+            self.insert(*item)
+        file.close()
 
-    def slice_prefix_and_cost(self):
-
-        f = open(self.route_cost_path, 'r')
-        for line in f:
-            splitted_line = line.strip().split(',')
-            prefix = splitted_line[0][1:]
-            cost = splitted_line[1]
-
-            self.populate_tree(prefix, cost)
-
-        f.close()
+    def insert(self, prefix, cost):
+        '''will add a whole prefix of integers to our tree'''
+        node = self.root
+        for i, num in enumerate(prefix):
+            if i <= len(prefix) - 1:
+                node.insert(num)
+                node = node.children[num]
+            else:
+                self.insert(num, cost)
